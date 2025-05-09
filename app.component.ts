3ea5c416955ba157d829b075151ca4b1f44ebc06
Viswateja06 <161558261+Viswateja06@users.noreply.github.com>
2025-05-09 22:13:55 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'foodies-hub';
+
+  popups() {
+    alert("Order is placed")
+  }
+}
